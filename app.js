650f70ef095e539008daa212e7d788b0f7aa0f16
Pascal Agbadi <pascalagbadi@gmail.com>
2021-06-26 11:46:05 +0000

    finished project

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const request = require('request');
+const cors = require('cors');
+require('dotenv').config()
+
+
+const port = process.env.PORT;
+const app = express();
+
+app.use(cors());
+
+app.use( (req,res,next)=>{
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+})
+
+//routing to the weather display
+app.get('/fetch_weather_data/:city', (req,res) => {
+  const location = req.params.city;
+  request(process.env.API+location, 
+  (err,req, body) => {
+    if(!err && res.statusCode==200){ 
+      res.send(body);
+    }
+  })
+});
+
+
+app.get('*', (req,res) =>{
+  res.sendStatus(404);
+});
+
+//start server
+app.listen(port, (req, res) => {  console.log( `server listening on port: ${port}`);})
+
